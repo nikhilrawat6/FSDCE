742d00ccb9822301343ac866de60a8318e823eef
nikhilrawat6 <nikhil.22b0151021@abes.ac.in>
2024-10-09 11:52:28 +0530

    Add files via upload

---
+++ Day1/jss.js
@@ -0,0 +1,17 @@
+function multiply(x,y){
+    return x*y;
+
+}
+module.exports = {
+   multiply
+}
+
+export function subtract(x,y) {
+
+}
+
+export const PI = 3.14159;
+
+export default function multiply(x,y){
+    return x * y;
+}
---
+++ Day1/main.js
@@ -0,0 +1,11 @@
+import {add,subtract,PI} from "./jss.js";
+
+import * as jadoo from "./jss.js";
+
+import multiply from "./jss.js";
+
+export { add,subtract,PI,multiply};
+console.log("Add: ",jadoo.add(5,3));
+console.log("Subtract: ", jadoo.subtract(5,3));
+console.log("PI: ", jadoo.PI);
+console.log("Multiply: ",multiply(5,3));
